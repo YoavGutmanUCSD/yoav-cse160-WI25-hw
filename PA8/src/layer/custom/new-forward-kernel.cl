d9a4a74c392d91821218b1d94bb201042df2c692
Yoav Gutman <ygutman@ucsd.edu>
2025-03-13 16:31:06 -0700

    first attempt

--- PA8/src/layer/custom/new-forward-kernel.cl
+++ PA8/src/layer/custom/new-forward-kernel.cl
@@ -8,9 +8,59 @@ __kernel void im2col(__global float *unrolled, __global float *x, const int B,
 #define x4d(i3, i2, i1, i0)                                                    \
   x[(i3) * (C_in * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
   // `unrolled` is a (B, H_unroll, W_unroll) tensor
+  // `unrolled` is a (B, C*K*K*(H-K+1), W-K+1) tensor
 #define x_unroll_3d(i2, i1, i0) unrolled[(i2 * H_unroll + i1) * W_unroll + i0]
+#define unroll_row(c,p,q) (c * K*K + p*K + q)
+#define unroll_col(h,w) (h*W_out+w)
+// usage: x_unroll_3d(b, unroll_col(c,p,q), unroll_row(h,w))
+
+/*#define x_unroll_3d(i2, i1, i0) unrolled[(i2*C*K*K + i1)*(H-K+1)*(W-K+1)+i0]*/
 
   //@@ Define your im2col operations here.
+  int W_out = (W-K+1);
+  int H_out = (H-K+1);
+  int H_unroll = K*K*C_in;
+  int W_unroll = H_unroll * H_out * W_out;
+  int maskRadius = K/2;
+
+  for(int b = 0; b < B; b++)
+  for(int cin = 0; cin < C_in; cin++)
+  for(int row_i = 0; row_i < H; row_i++)
+  for(int col_i = 0; col_i < W; col_i++)
+  for(int p = 0; p < K; p++)
+  for(int q = 0; q < K; q++)
+  {
+    int row_o = row_i - maskRadius;
+    int col_o = col_i - maskRadius;
+    _Bool row_o_in_bounds = row_o >= 0 && row_o < H-K+1;
+    _Bool col_o_in_bounds = col_o >= 0 && col_o < W-K+1;
+    if(row_o_in_bounds && col_o_in_bounds)
+    {
+      int row_u = unroll_row(cin, p, q);
+      int col_u = unroll_col(row_i, col_i);
+      x_unroll_3d(b, col_u, row_u) = x4d(b, cin, row_i, col_i);
+    }
+  }
+
+	/* sequential pseudocode
+for b in 0..<B:
+    for c_in in 0..<C:
+        for row_i in 0..<H:
+            for col_i in 0..<W:
+                for mask_offset_row in 0..<K:
+                    for mask_offset_col in 0..<K:
+                        # indices in the output of the convolution whose receptive fields include (row_i, col_i)
+                        row_o = ??
+                        col_o = ??
+                        row_o_in_bounds = 0 <= row_o and row_o < H - K + 1
+                        col_o_in_bounds = 0 <= col_o and col_o < W - K + 1
+                        if row_o_in_bounds and col_o_in_bounds:
+                            # indices in x_unroll to write to
+                            col_u = ??
+                            row_u = ??
+
+                            x_unroll[b, col_u, row_u] = x[b, c_in, row_i, col_i]
+     */
 
 #undef x4d
 #undef x_unroll_3d
--- PA8/src/layer/custom/new-forward.cc
+++ PA8/src/layer/custom/new-forward.cc
@@ -16,11 +16,77 @@
         exit(EXIT_FAILURE);                            \
     }
 
+#define will_debug 1
+#define PRINT if(will_debug) printf
+
 void OpenCLInterface::conv_forward_gemm_opencl_prolog(const float *host_y, const float *host_x, const float *host_k, cl_mem *device_y, cl_mem *device_x, cl_mem *device_k, cl_mem *device_x_unroll, const int B, const int M, const int C, const int H, const int W, const int K)
 {
     //@@ Allocate GPU memory here (don't forget batch sizes!)
+	size_t input_size  = B * C * H * W; // indexed in this order
+	size_t kernel_size = M * C * K * K;
+	size_t unroll_size = B * C * K * K * (H-K+1) * (W-K+1);
+	size_t output_size = B * M * (H-K+1) * (W-K+1);
+	cl_int err;
+	*device_x = clCreateBuffer(
+			this->opencl->context
+			, CL_MEM_READ_ONLY
+			, input_size * sizeof(float)
+			, NULL // host_ptr points to already allocated memory. we have none
+			, &err
+			);
+	PRINT("allocated device_x (image)\n");
+	*device_k = clCreateBuffer(
+			this->opencl->context
+			, CL_MEM_READ_ONLY
+			, kernel_size * sizeof(float)
+			, NULL // host_ptr points to already allocated memory. we have none
+			, &err
+			);
+	PRINT("allocated device_k (mask)\n");
+
+	*device_y = clCreateBuffer(
+			this->opencl->context
+			, CL_MEM_WRITE_ONLY
+			, output_size * sizeof(float)
+			, NULL // host_ptr points to already allocated memory. we have none
+			, &err
+			);
+	PRINT("allocated device_y (final output)\n");
+
+	*device_x_unroll = clCreateBuffer(
+			this->opencl->context
+			, CL_MEM_READ_WRITE
+			, unroll_size * sizeof(float)
+			, NULL // host_ptr points to already allocated memory. we have none
+			, &err
+			);
+	PRINT("allocated device_x_unroll (intermediate output)\n");
 
     //@@ Copy memory to the GPU here
+	clEnqueueWriteBuffer(
+			this->opencl->queue
+			, *device_x
+			, CL_TRUE // maybe CL_FALSE possible -- try
+			, 0
+			, input_size * sizeof(float)
+			, host_x
+			, 0
+			, NULL
+			, NULL
+			);
+	PRINT("enqueue write to device_x\n");
+	clEnqueueWriteBuffer(
+			this->opencl->queue
+			, *device_k
+			, CL_TRUE // maybe CL_FALSE possible -- try
+			, 0
+			, kernel_size * sizeof(float)
+			, host_k
+			, 0
+			, NULL
+			, NULL
+			);
+	PRINT("enqueue write to device_k\n");
 }
 
 void OpenCLInterface::conv_forward_gemm_opencl(cl_mem device_y, const cl_mem device_x, const cl_mem device_k, const cl_mem device_x_unroll, const int B, const int M, const int C, const int H, const int W, const int K)
@@ -28,14 +94,94 @@ void OpenCLInterface::conv_forward_gemm_opencl(cl_mem device_y, const cl_mem dev
     //@@ ====== Start im2col =====
 
     // @@ define local and global work sizes
+	size_t local_work_size[3] = {0,0,0};
+	size_t global_work_size[3] = {1,1,1};
+
+	cl_int err;
+    err = clSetKernelArg(this->opencl->im2col_kernel,  0, sizeof(cl_mem), &device_x_unroll);
+    CHECK_ERR(err, "clSetKernelArg 0");
+    err |= clSetKernelArg(this->opencl->im2col_kernel, 1, sizeof(cl_mem), &device_x);
+    CHECK_ERR(err, "clSetKernelArg 1");
+    err |= clSetKernelArg(this->opencl->im2col_kernel, 2, sizeof(int), &B);
+    CHECK_ERR(err, "clSetKernelArg 2");
+    err |= clSetKernelArg(this->opencl->im2col_kernel, 3, sizeof(int), &C);
+    CHECK_ERR(err, "clSetKernelArg 3");
+    err |= clSetKernelArg(this->opencl->im2col_kernel, 4, sizeof(int), &H);
+    CHECK_ERR(err, "clSetKernelArg 4");
+    err |= clSetKernelArg(this->opencl->im2col_kernel, 5, sizeof(int), &W);
+    CHECK_ERR(err, "clSetKernelArg 5");
+    err |= clSetKernelArg(this->opencl->im2col_kernel, 6, sizeof(int), &K);
+    CHECK_ERR(err, "clSetKernelArg 6");
 
     //@@ Launch the im2col kernel here
+	err = clEnqueueNDRangeKernel(
+			this->opencl->queue
+			, this->opencl->im2col_kernel
+			, 3 // work dimension, it's 3d this time around
+			, NULL
+			, global_work_size
+			/*, &just_one*/
+			/*, local_work_size*/
+			/*, &just_one*/
+			, NULL // locwrksize
+			, 0
+			, NULL
+			, NULL);
 
     //@@ ====== End im2col =====
 
     //@@ ====== Start gemm =====
 
     // @@ Call clblast::GemmBatched here
+	auto unroll_offsets = std::vector<size_t>();
+	auto output_offsets = std::vector<size_t>();
+	std::vector<size_t> alphas(B, 1);
+	std::vector<size_t> betas(B, 0);
+	size_t single_unrolled_size = (W-K+1) * (H-K+1) * C * K * K;
+	size_t single_output_size = (W-K+1) * (H-K+1);
+	size_t* alphas_data = alphas.data();
+	size_t* betas_data = betas.data();
+	for(int i = 0; i < B; i++)
+	{
+		unroll_offsets.push_back(i * single_unrolled_size);
+		output_offsets.push_back(i * single_output_size);
+	}
+	// Mx(K*K)∙(K*K)x((W-K+1)(H-K+1)) = Mx((W-K+1)(H-K+1)), which is desired.
+	// can't make K*K the columns of B.
+	// solution: transpose both and do B∙A. then do another transpose. 
+	// this would be FUN. but maybe for LATER.
+
+	clblast::GemmBatched<size_t*>(
+			clblast::Layout::kRowMajor   // const Layout layout
+			, clblast::Transpose::kNo    // const Transpose a_transpose
+			, clblast::Transpose::kNo    // const Transpose b_transpose
+			, M                          // const size_t m		rows in A. 
+			, (W-K+1)*(H-K+1)            // const size_t n		columns in B. 
+			, K*K*C                      // const size_t k		rows in B and columns in A.
+			, &alphas_data                // const T *alphas
+			, device_k                   // const cl_mem a_buffer
+			, betas.data()               // const size_t *a_offsets
+			, K*K*C                      // const size_t a_ld
+			, device_x_unroll            // const cl_mem b_buffer
+			, unroll_offsets.data()      // const size_t *b_offsets
+			, (W-K+1)*(H-K+1)            // const size_t b_ld
+			, &betas_data                 // const T *betas
+			, device_y                   // cl_mem c_buffer
+			, output_offsets.data()      // const size_t *c_offsets
+			, M                          // const size_t c_ld
+			, B                          // const size_t batch_count
+			, &this->opencl->queue       // cl_command_queue* queue
+			, NULL                       // cl_event* event
+			);
+/*GemmBatched(const Layout layout, const Transpose a_transpose, const Transpose b_transpose,*/
+/*                       const size_t m, const size_t n, const size_t k,*/
+/*                       const T *alphas,*/
+/*                       const cl_mem a_buffer, const size_t *a_offsets, const size_t a_ld,*/
+/*                       const cl_mem b_buffer, const size_t *b_offsets, const size_t b_ld,*/
+/*                       const T *betas,*/
+/*                       cl_mem c_buffer, const size_t *c_offsets, const size_t c_ld,*/
+/*                       const size_t batch_count,*/
+/*                       cl_command_queue* queue, cl_event* event)*/
 
     //@@ ====== End gemm =====
 }
